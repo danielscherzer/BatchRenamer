6b82eac2aceba20790fc366e6c423bd58b148fd1
scherzer <scherzer@hs-weingarten.de>
2020-06-15 23:06:45 +0200

    quick commit

--- BatchRenamer/Properties/Settings.Designer.cs
+++ BatchRenamer/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace BatchRenamer.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -107,18 +107,15 @@ namespace BatchRenamer.Properties {
                 this["IgnoreExtension"] = value;
             }
         }
-
+        
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string Counter
-        {
-            get
-            {
+        [global::System.Configuration.DefaultSettingValueAttribute("001")]
+        public string Counter {
+            get {
                 return ((string)(this["Counter"]));
             }
-            set
-            {
+            set {
                 this["Counter"] = value;
             }
         }
