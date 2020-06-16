d9bd9d20d87ef1897560e583a59170e031a4b853
scherzer <scherzer@hs-weingarten.de>
2020-06-16 14:26:21 +0200

    csproj version patching

--- BatchRenamerTests/FileRenameOperationsTests.cs
+++ BatchRenamerTests/FileRenameOperationsTests.cs
@@ -1,41 +1,24 @@
-﻿using BatchRenamer;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BatchRenamer.Tests
 {
 	[TestClass()]
 	public class FileRenameOperationsTests
 	{
-		[TestMethod()]
-		public void FormatYearTestNothingToDo()
+		[DataTestMethod()]
+		[DataRow("3 Ninjas - High Noon at Mega Mountain (1998) (1080p) (HD)(WS)(EN)(GE).mkv",
+				 "3 Ninjas - High Noon at Mega Mountain (1998) (1080p) (HD)(WS)(EN)(GE).mkv")]
+		[DataRow("3 Ninjas - High Noon at Mega Mountain 1998 (1080p) (HD)(WS)(EN)(GE).mkv",
+				 "3 Ninjas - High Noon at Mega Mountain (1998) (1080p) (HD)(WS)(EN)(GE).mkv")]
+		[DataRow("3 1980 Ninjas - High Noon at Mega Mountain 1998 (1080) (HD)(WS)(EN)(GE).mkv",
+				 "3 1980 Ninjas - High Noon at Mega Mountain (1998) (1080) (HD)(WS)(EN)(GE).mkv")]
+		[DataRow("3 1980 Ninjas - High Noon at Mega Mountain 1898 (1080) (HD)(WS)(EN)(GE).mkv", 
+			     "3 (1980) Ninjas - High Noon at Mega Mountain 1898 (1080) (HD)(WS)(EN)(GE).mkv")]
+		public void FormatYearTest(string input, string expectedOutput)
 		{
-			var input = "3 Ninjas - High Noon at Mega Mountain (1998) (1080p) (HD)(WS)(EN)(GE).mkv";
-			Assert.AreEqual(input.FormatYear(), input);
-		}
-
-		[TestMethod()]
-		public void FormatYearTest1()
-		{
-			var input = "3 Ninjas - High Noon at Mega Mountain 1998 (1080p) (HD)(WS)(EN)(GE).mkv";
-			var expectedOutput = "3 Ninjas - High Noon at Mega Mountain (1998) (1080p) (HD)(WS)(EN)(GE).mkv";
 			Assert.AreEqual(input.FormatYear(), expectedOutput);
 		}
 
-		[TestMethod()]
-		public void FormatYearTwoYearsTest()
-		{
-			var input = "3 1980 Ninjas - High Noon at Mega Mountain 1998 (1080) (HD)(WS)(EN)(GE).mkv";
-			var expectedOutput = "3 1980 Ninjas - High Noon at Mega Mountain (1998) (1080) (HD)(WS)(EN)(GE).mkv";
-			Assert.AreEqual(input.FormatYear(), expectedOutput);
-		}
-
-		[TestMethod()]
-		public void FormatYearTwoYearsSecondNoMovieYearTest()
-		{
-			var input = "3 1980 Ninjas - High Noon at Mega Mountain 1898 (1080) (HD)(WS)(EN)(GE).mkv";
-			var expectedOutput = "3 (1980) Ninjas - High Noon at Mega Mountain 1898 (1080) (HD)(WS)(EN)(GE).mkv";
-			Assert.AreEqual(input.FormatYear(), expectedOutput);
-		}
 
 		[TestMethod()]
 		public void FormatWordsTest()
